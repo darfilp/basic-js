907eac475f30d5b79505a2cee6047d3ae0d18db2
Daria <wwdarcool@yandex.ru>
2022-05-01 14:51:13 +0300

    add task carbon-dating

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,12 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+
 
 module.exports = {
   DepthCalculator
