c5137ecc26cc1dfe2fe99e5b85a100db21acd34a
Daria <wwdarcool@yandex.ru>
2022-05-01 17:49:34 +0300

    fix transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,32 +14,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  let transformed = [];
+  let array = [];
   if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === '--double-next') {
       if (i != arr.length - 1) {
-          transformed.push(arr[i + 1]);
+          array.push(arr[i + 1]);
         }
     } else if (arr[i] === '--double-prev') {
       if (arr[i - 2] != "--discard-next" && i != 0) {
-        transformed.push(arr[i - 1]);
+        array.push(arr[i - 1]);
       } 
     } else if (arr[i] === '--discard-prev') {
       if (arr[i - 2] != "--discard-next" && i != 0) {
-        transformed.pop(); 
+        array.pop(); 
       }
     } else if (arr[i] === '--discard-next') {
       if (i != arr.length - 1) {
         i++;
       }
     } else {
-      transformed.push(arr[i]);
+      array.push(arr[i]);
     }
   } 
-  console.log(transformed)
-  return transformed;
+  console.log(array)
+  return array;
 }
 
 module.exports = {
