4a4055996290e25ed95bb473b1cb3fe40439a0ae
Daria <wwdarcool@yandex.ru>
2022-05-01 15:50:45 +0300

    add task common

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,8 +12,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  for (let i of s1) {
+    s2 = s2.replace(i, '^')
+  }
+  return s2.split('').filter(item => item === '^').length;
 }
 
 module.exports = {
